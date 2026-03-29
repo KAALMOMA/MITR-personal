3cc7a57cddfe518674822400ed39faa9260dc1d2
Katrina Mosquera-Maykovych <katrinamosquera@gmail.com>
2026-03-28 20:45:43 -0400

    Updated styling, removed broken cubes background

--- my-js-site/src/app/page.js
+++ my-js-site/src/app/page.js
@@ -1,5 +1,3 @@
-import RandomCubesBg from "../../components/RandomCubesBg";
-
 export default function Home() {
   const navlinks = [
     { label: "About", url: "https://example.com" },
@@ -13,10 +11,8 @@ export default function Home() {
 
   return (
     <main style={styles.container}>
-      <RandomCubesBg />
       <div style={styles.navbar}>
         <img src="fbx_logo.png" alt="FBX Logo" style={styles.logo} />
-        <script src="/scripts/RandomCubesBg.min.js" data-nscript="afterInteractive"></script>
         <div style={styles.navlinksContainer}>
           {navlinks.map((link, index) => (
             <a key={index} href={link.url} target="_blank" rel="noopener noreferrer" style={styles.navlinks}>
@@ -31,8 +27,8 @@ export default function Home() {
         </div>
       </div>
       
-      <div style={styles.hero}>
-        <div style={styles.heroLeft}>
+      <div style={styles.heroMain}>
+        <div style={styles.heroMainLeft}>
           <a href="https://example.com" target="_blank" style={styles.glassButton}>Visit the FBX Shop</a>
           <img src="fbx_logo.png" alt="Hero Image" style={{ maxHeight: "40vh" }} />
           <div style={styles.heroTitle}>The ultimate kick-starter to becoming a roboticist</div>
@@ -48,82 +44,91 @@ export default function Home() {
         <div style={styles.heroRight}>
           <img src="fbx_logo.png" alt="Hero Image" style={{ borderRadius: "8px", maxHeight: "300px", maxWidth: "100%" }} />
         </div>
+
+      <div style={styles.space}></div>
         
       </div>
-      <div style={styles.hero}>
-        <div style={styles.heroLeft}>
-          <div style={styles.stackTitle}>Build Assembly Skills</div>
-          <p style={styles.heroText}>Build Assembly Skills and Learn the Anatomy of A Robot</p>
-          <div style={styles.buttonContainer}>
-            <a href="https://example.com" target="_blank"style={styles.gradientButton}>Learn More</a>
+      <div style={styles.restofPage}>
+        <div style={styles.hero}>
+          <div style={styles.heroLeft}>
+            <div style={styles.stackTitle}>Build Assembly Skills</div>
+            <p style={styles.heroText}>Build Assembly Skills and Learn the Anatomy of A Robot</p>
+            <div style={styles.buttonContainer}>
+              <a href="https://example.com" target="_blank"style={styles.gradientButton}>Learn More</a>
+            </div>
+          </div>
+          <div style={styles.heroRight}>
+            <img src="fbx_logo.png" alt="Hero Image" style={{ borderRadius: "8px", maxHeight: "500px", maxWidth: "100%" }} />
           </div>
         </div>
-        <div style={styles.heroRight}>
-          <img src="fbx_logo.png" alt="Hero Image" style={{ borderRadius: "8px", maxHeight: "500px", maxWidth: "100%" }} />
-        </div>
-      </div>
 
-      <div style={styles.hero}>
-        <div style={styles.heroLeft}>
-          <img src="fbx_logo.png" alt="Hero Image" style={{ borderRadius: "8px", maxHeight: "500px", maxWidth: "100%" }} />
-        </div>
-        <div style={styles.heroRight}>
-          <div style={styles.stackTitle}>Step-by-Step Learning</div>
-          <p style={styles.heroText}>All Taught Through Online Step-By-Step Guides and Video Tutorials</p>
-          <div style={styles.buttonContainer}>
-            <a href="https://example.com" target="_blank"style={styles.gradientButton}>Learn More</a>
+        <div style={styles.hero}>
+          <div style={styles.heroLeft}>
+            <img src="fbx_logo.png" alt="Hero Image" style={{ borderRadius: "8px", maxHeight: "500px", maxWidth: "100%" }} />
+          </div>
+          <div style={styles.heroRight}>
+            <div style={styles.stackTitle}>Step-by-Step Learning</div>
+            <p style={styles.heroText}>All Taught Through Online Step-By-Step Guides and Video Tutorials</p>
+            <div style={styles.buttonContainer}>
+              <a href="https://example.com" target="_blank"style={styles.gradientButton}>Learn More</a>
+            </div>
           </div>
         </div>
-      </div>
 
-      <div style={styles.hero}>
-        <div style={styles.heroLeft}>
-          <div style={styles.stackTitle}>Test Your Skills</div>
-          <p style={styles.heroText}>Test Your Robotics Skills in Capstones and Challenges</p>
-          <div style={styles.buttonContainer}>
-            <a href="https://example.com" target="_blank"style={styles.gradientButton}>Learn More</a>
+        <div style={styles.hero}>
+          <div style={styles.heroLeft}>
+            <div style={styles.stackTitle}>Test Your Skills</div>
+            <p style={styles.heroText}>Test Your Robotics Skills in Capstones and Challenges</p>
+            <div style={styles.buttonContainer}>
+              <a href="https://example.com" target="_blank"style={styles.gradientButton}>Learn More</a>
+            </div>
+          </div>
+          <div style={styles.heroRight}>
+            <img src="fbx_logo.png" alt="Hero Image" style={{ borderRadius: "8px", maxHeight: "500px", maxWidth: "100%" }} />
           </div>
         </div>
-        <div style={styles.heroRight}>
-          <img src="fbx_logo.png" alt="Hero Image" style={{ borderRadius: "8px", maxHeight: "500px", maxWidth: "100%" }} />
-        </div>
-      </div>
 
-      <div style={styles.hero}>
-        <div style={styles.heroLeft}>
-          <img src="fbx_logo.png" alt="Hero Image" style={{ borderRadius: "8px", maxHeight: "500px", maxWidth: "100%" }} />
-        </div>
-        <div style={styles.heroRight}>
-          <div style={styles.stackTitle}>Learn to Code</div>
-          <p style={styles.heroText}>Learn to Code in the World of Robotics Autonomous and Driver Control</p>
-          <div style={styles.buttonContainer}>
-            <a href="https://example.com" target="_blank"style={styles.gradientButton}>Learn More</a>
+        <div style={styles.hero}>
+          <div style={styles.heroLeft}>
+            <img src="fbx_logo.png" alt="Hero Image" style={{ borderRadius: "8px", maxHeight: "500px", maxWidth: "100%" }} />
+          </div>
+          <div style={styles.heroRight}>
+            <div style={styles.stackTitle}>Learn to Code</div>
+            <p style={styles.heroText}>Learn to Code in the World of Robotics Autonomous and Driver Control</p>
+            <div style={styles.buttonContainer}>
+              <a href="https://example.com" target="_blank"style={styles.gradientButton}>Learn More</a>
+            </div>
           </div>
         </div>
-      </div>
-
-      <div style={styles.spotlightStack}>
-        <div style={styles.heroTitle}>We Prioritize Access</div>
-        <p style={styles.heroText}>Our team strives to democratize experiential engineering education.</p>
-        <div style={styles.spotlightCardContainer}>
-          <div style={styles.spotlightCard}>
-            <div style={styles.spotlightCardTitle}>Open-Source to the Core</div>
-            <p style={styles.spotlightCardText}>The FBX Rover is an open-source robotics platform built using an ESP-32 microcontroller, allowing users to access and modify its underlying code and hardware to support customization, experimentation, and hands-on learning in robotics and embedded systems.</p>
-          </div>
-          <div style={styles.spotlightCard}>
-            <div style={styles.spotlightCardTitle}>Scholarships</div>
-            <p style={styles.spotlightCardText}>We offer scholarships to ensure that financial barriers do not hinder access to our programs.</p>
-          </div>
-        </div>
-      </div>
-
-      <div style={styles.floatingStack}>
-        <div style={styles.heroTitle}>Our Partners</div>
         
-      </div>  
+        <div style={styles.space}></div>
+        
+        <div style={styles.spotlightStack}>
+          <div style={styles.heroTitle}>We Prioritize Access</div>
+          <p style={styles.heroText}>Our team strives to democratize experiential engineering education.</p>
+          <div style={styles.spotlightCardContainer}>
+            <div style={styles.spotlightCard}>
+              <div style={styles.spotlightCardTitle}>Open-Source to the Core</div>
+              <p style={styles.spotlightCardText}>The FBX Rover is an open-source robotics platform built using an ESP-32 microcontroller, allowing users to access and modify its underlying code and hardware to support customization, experimentation, and hands-on learning in robotics and embedded systems.</p>
+            </div>
+            <div style={styles.spotlightCard}>
+              <div style={styles.spotlightCardTitle}>Scholarships</div>
+              <p style={styles.spotlightCardText}>We offer scholarships to ensure that financial barriers do not hinder access to our programs.</p>
+            </div>
+          </div>
+        </div>
 
-      <div style={styles.buttonContainer}>
-        <div style={styles.gradientButton}>Get Started</div>
+        <div style={styles.space}></div>
+
+        <div style={styles.floatingStack}>
+          <div style={styles.heroTitle}>Our Partners</div>
+          
+        </div>  
+
+        <div style={styles.buttonContainer}>
+          <div style={styles.gradientButton}>Get Started</div>
+        </div>
+        <div style={styles.space}></div>
       </div>
     </main>
   );
@@ -137,6 +142,21 @@ const styles = {
     alignItems: "center",
     fontFamily: "Arial, sans-serif",
     overflow: "visible",
+    backgroundImage: "url('/color4bg_cubes_alt.png')",
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+    backgroundRepeat: "no-repeat",
+    backgroundAttachment: "fixed",
+    padding: "0px 0px 0px 0px",
+  },
+  restofPage: {
+    width: "100%",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    overflow: "visible",
+    backgroundColor: "#0b081c",
+    gap: "20px",
   },
   navbar: {
     height: "200px",
@@ -150,7 +170,7 @@ const styles = {
     backgroundColor: "blue",
     padding: "10px 20px",
     overflow: "visible",
-    backgroundColor: "navy",
+    backgroundColor: "#0b081c",
   },
   title: {
     fontSize: "2rem",
@@ -163,6 +183,7 @@ const styles = {
     padding: "10px 20px",
   },
   navlinksContainer: {
+    fontFamily: "var(--font-montserrat), Arial, sans-serif",
     display: "flex",
     gap: "10px",
     justifyContent: "center",
@@ -170,6 +191,7 @@ const styles = {
     overflow: "visible",
   },
   roverlinksContainer: {
+    fontFamily: "var(--font-montserrat), Arial, sans-serif",
     display: "flex",
     flexDirection: "row",
     gap: "10px",
@@ -195,6 +217,7 @@ const styles = {
     marginRight: "10px",
   },
   button: {
+    fontFamily: "var(--font-montserrat), Arial, sans-serif",
     padding: "10px 20px",
     fontSize: "1rem",
     cursor: "pointer",
@@ -204,6 +227,7 @@ const styles = {
     color: "white",
   },
   whitebutton: {
+    fontFamily: "var(--font-montserrat), Arial, sans-serif",
     padding: "10px 20px",
     fontSize: "1rem",
     cursor: "pointer",
@@ -213,15 +237,17 @@ const styles = {
     color: "#0070f3",
   },
   gradientButton: {
+    fontFamily: "var(--font-montserrat), Arial, sans-serif",
     padding: "10px 20px",
     fontSize: "1rem",
     cursor: "pointer",
     borderRadius: "2em",
     border: "2px solid transparent",
-    background: "linear-gradient(navy, navy) padding-box, linear-gradient(45deg, #00ff8c, #f06595) border-box",
+    background: "linear-gradient(#0b081c) padding-box, linear-gradient(45deg, #629fcc, #c975b9) border-box",
     color: "white",
   },
   glassButton: {
+    fontFamily: "var(--font-montserrat), Arial, sans-serif",
     padding: "10px 20px",
     fontSize: "1rem",
     cursor: "pointer",
@@ -230,16 +256,39 @@ const styles = {
     backgroundColor: "rgba(255, 255, 255, 0.1)",
     color: "white",
   },
-  hero: {
+  heroMain: {
     width: "100%",
     height: "auto",
-    backgroundColor: "#f0f0f03c",
+    backgroundImage: "linear-gradient(to bottom, #0e5c7b00, #0e5c7b00, #0e5c7b00, #0e5c7b00, #13173e, #0b081c)",
     display: "flex",
     alignItems: "center",
     justifyContent: "center",
+    flexDirection: "row",
+    padding: "0px 20px",
+  },
+  hero: {
+    width: "100%",
+    height: "auto",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#0b081c",
     marginBottom: "20px",
     flexDirection: "row",
     padding: "0px 20px",
+  },
+  space: {
+    height: "50px",
+  },
+  heroMainLeft: {
+    flex: 1,
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    padding: "20px",
+    textAlign: "center",
+    height: "90vh",
   },
   heroLeft: {
     flex: 1,
@@ -249,7 +298,7 @@ const styles = {
     justifyContent: "center",
     padding: "20px",
     textAlign: "center",
-    height: "90vh",
+    height: "50vh",
   },
   heroRight: {
     flex: 1,
@@ -301,6 +350,7 @@ const styles = {
     whiteSpace: "normal",
   },
   spotlightStack: {
+    fontFamily: "var(--font-montserrat), Arial, sans-serif",
     display: "flex",
     width: "60%",
     flexDirection: "column",
